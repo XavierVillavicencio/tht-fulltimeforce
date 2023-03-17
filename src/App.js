5a1301dfeb6a9a609609211cfb024af3c2b29e88
Xavier Villavicencio B <xavier.villavicencio.b@gmail.com>
2023-03-16 21:45:24 -0500

    Typescript configuration file update
    Tailwind configuration added; additionally google fonts import was added to customize the design.
    The included main.js file is placed as a base for the other layout files.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,10 +5,10 @@ function App() {
     <div className="App">
      
       <section>
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="text-3xl font-bold underline font-sans">
           Hello world!
         </h1>
-        <div class="text-lg md:text-xl bg-red-500 hover:bg-blue-500">
+        <div class="text-lg md:text-xl bg-red-500 hover:bg-blue-500 font-serif">
           Something in the middle
         </div>
       </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      padding: '2rem',
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+      serif: ['Roboto', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
