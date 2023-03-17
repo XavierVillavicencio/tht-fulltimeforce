dd0dfdbd1264ca1d1fabf434cf2c4e7c1863db6a
Xavier Villavicencio B <xavier.villavicencio.b@gmail.com>
2023-03-16 21:21:17 -0500

    Tailwindcss configuration added
    We add the tailwindcss configuration file to start using it.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <section>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <div class="text-lg md:text-xl bg-red-500 hover:bg-blue-500">
+          Something in the middle
+        </div>
+      </section>
     </div>
   );
 }
